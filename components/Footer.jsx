6fc16d0969437ed57c6bf05d06b828415f891427
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-09 12:09:37 +0100

    feat: complete footer component design

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { GiSelfLove } from "react-icons/gi";
 
 const Footer = () => {
-  return <div className="container px-20">Footer</div>;
+  return (
+    <div className="container px-24 py-3 text-gray-100 bg-red-500 mt-5 grid grid-cols-3">
+      <span className="col-span-2 ">copyright, 2023. All rights reserved</span>
+      <span className="flex items-center gap-2">
+        made with <GiSelfLove /> by{" "}
+        <span className="font-bold cursor-pointer hover:text-blue-400">
+          &lt;Dhulnurein /&gt;
+        </span>
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
